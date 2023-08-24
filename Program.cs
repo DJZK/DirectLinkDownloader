9860dfb8b8025716a049439eabac1240bc4ed389
DJZK <emperorxizzle99@gmail.com>
2023-08-24 15:14:41 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectLinkDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: DirectLinkDownloader.exe <URL>");
+                return;
+            }
+
+            string url = args[0];
+            string fileName = url.Substring(url.LastIndexOf("/") + 1);
+
+            using (WebClient client = new WebClient())
+            {
+                Console.WriteLine($"Downloading {fileName}...");
+                client.DownloadFile(url, fileName);
+                Console.WriteLine("Download complete.");
+            }
+        }
+    }
+}
